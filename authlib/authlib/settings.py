22f3179cb1179768e439cc9098eb0e1107dfc79e
aisfak07 <isfakahmed163@gmail.com>
2024-12-15 14:23:09 +0300

    Last Commit Hope so

--- authlib/authlib/settings.py
+++ authlib/authlib/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'authlib.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'authlibdb',
-        'USER': 'admin',
-        'PASSWORD': 'isfak98',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
